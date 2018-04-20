95682f0a9bd4c2bbf452295b927c9f9cab6686ef
piotrkania <36471063+piotrkania@users.noreply.github.com>
2018-04-20 14:25:38 +0200

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 from random import randint
-
+from professions import *
 
 # Defines die roll
+
 class Die:
     def __init__(self, sides=6):
         self.sides = sides
@@ -11,7 +12,10 @@ class Die:
     def roll(self):
         return randint(1, self.sides)
 
-four_sided_die=Die(4)
-six_sided_die=Die(6)
-ten_sided_die=Die(10)
-twenty_sided_die=Die(20)
+# Die types
+
+level_up_die = Die(hero.HD)
+four_sided_die = Die(4)
+six_sided_die = Die(6)
+ten_sided_die = Die(10)
+twenty_sided_die = Die(20)
