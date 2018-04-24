1b4ea32131a2072f94b26c9dd01f67daaf23d6ad
piotrkania <36471063+piotrkania@users.noreply.github.com>
2018-04-24 12:34:55 +0200

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
+import random
 from dice import *
 from professions import *
 from monsters import *
+import time
 
 def PlayerAttack():
     roll=twenty_sided_die.roll()
     if roll >= hero.thaco - mob.ac:
-        rollD = hero.MAX_POWER
+        rollD = random.choice(hero.POWER)
         print(hero.name + " rolled " + str(roll) + " and  hit " + mob.name + " for " + str(rollD) + " damage.")
         print("")
         mob.hp -= rollD
@@ -19,10 +21,10 @@ def PlayerAttack():
 
 
 def MonsterAttack():
-    roll=ten_sided_die.roll()
-    if roll >= mob.thaco-hero.ac:
-        rollD = mob.MAX_POWER
-        print(mob.name + " rolled " + str(roll) + " and hits You for " + str(rollD) + "." )
+    roll=twenty_sided_die.roll()
+    if roll >= mob.thaco - hero.ac:
+        rollD = random.choice(mob.POWER)
+        print(mob.name + " rolled " + str(roll) + " and hits You for " + str(rollD) + " damage." )
         print("")
         hero.hp -= rollD
         print(hero.name + " has " + str(hero.hp) + " hp left.")
@@ -31,24 +33,20 @@ def MonsterAttack():
         print(mob.name + " rolled " + str(roll) + " and missed.")
         print("")
 
-while True:
-    if command:
-        hero.COMMANDS[command][1]()
-        PlayerAttack()
-        time.sleep(1)
-        if mob.hp > 0:
-            MonsterAttack()
-            time.sleep(1)
-            if hero.hp <= 0:
-                print("++++++You were killed++++++")
-                time.sleep(1)
-                break
-            else:
-                continue
-        elif mob.hp <= 0 and hero.hp > 0 :
-            print("++++++You killed " + mob.name+ "++++++")
+def Level_Up():
+    while hero.exp >= hero.LEVEL_2:
+        levelGain = False
+        hero.LEVEL += 1
+        levelGain = True
+        hero.LEVEL_2 = hero.LEVEL_2*2
+        if levelGain == True:
+            hp_gain = level_up_die.roll()
+            hero.MAX_HP += hp_gain
+            print("LEVEL UP!!! Gained " + str(hp_gain) + "HP.")
             print("")
+            hero.hp = hero.MAX_HP
+            print("Name: {}, HP: {}, LEVEL: {}\n".format(
+                  hero.name, hero.hp, hero.LEVEL))
+        else :
+            hero.exp += mob.exp
             print("Gained " + str(mob.exp) + " XP.")
-            time.sleep(1)
-            break
-    Level_Up()
