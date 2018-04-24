a9ba62a19f92065d5665ce2de619689467918b1b
piotrkania <36471063+piotrkania@users.noreply.github.com>
2018-04-24 12:32:49 +0200

    Update monsters.py

--- monsters.py
+++ monsters.py
@@ -15,28 +15,54 @@ class Zombie(Monster):
     def __init__(self):
         super().__init__(name="Zombie",
                          hp=10,ac=5,
-                         exp=5,thaco=20)
+                         exp=4,thaco=20)
+
+    POWER=[1,2,3,4,5,6,7]
 
 class Ghul(Monster):
     def __init__(self):
         super().__init__(name="Ghul",
                          hp=12,ac=6,
-                         exp=4,thaco=20)
+                         exp=1,thaco=20)
+
+    POWER=[1,2,3,4,5,6]
 
 class Skeleton(Monster):
     def __init__(self):
         super().__init__(name="Skeleton",
                          hp=6,ac=2,
-                         exp=2,thaco=20)
+                         exp=1,thaco=20)
+
+    POWER=[1,2,3,4]
 
 class Ghost(Monster):
     def __init__(self):
         super().__init__(name="Ghost",
                          hp=5,ac=10,
-                         exp=5,thaco=20)
+                         exp=2,thaco=20)
+
+    POWER=[1,2,3,4,5,6]
+
+
+class Slime(Monster):
+    def __init__(self):
+        super().__init__(name="Slime",
+                         hp=26,ac=8,
+                         exp=4,thaco=20)
+
+    POWER=[5,6,7,8,9,10]
 
 def random_mob():
-    mob=Zombie() if four_sided_die.roll()>2 else Ghul()
-    return mob
+    roll = twenty_sided_die.roll()
+    if roll <= 5 :
+        return Zombie()
+    elif roll <= 10:
+        return Ghul()
+    elif roll <= 15:
+        return Skeleton()
+    elif roll <= 19:
+        return Ghost()
+    else:
+        return Slime()
 
 mob = random_mob()
