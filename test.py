d285547e491d95698df2cd196faa266558ee6a7a
piotrkania <36471063+piotrkania@users.noreply.github.com>
2018-04-30 13:53:45 +0200

    Update test.py

--- test.py
+++ test.py
@@ -12,43 +12,50 @@ import spells
 # Profession selection
 
 print("Name: {}, HP: {}, AC: {}, EXP: {}, ATK: {}\n".format(
-      hero.name, hero.hp, hero.ac, hero.exp, hero.thaco))
+      hero.name, hero.hp, hero.ac, hero.EXP, hero.thaco))
 
 
-# Enter the building
-for command, action in hero.COMMANDS.items():
-    print("Press {} to {}".format(command, action[0]))
+# Mob encounter
 
-while True:
-    command = input("~~~~~~~Press key to continue~~~~~~~")
-    if command not in hero.COMMANDS:
-        print("Not a valid command")
-        continue
-    print("You are fighting "  + mob.name)
-    print("")
-    time.sleep(1)
-    break
+print("You encountered a monster!!! Defend Yourself")
 
-while True:
-    if command:
-        hero.COMMANDS[command][1]()
-        PlayerAttack()
+
+# Fight mob
+
+def encounter():
+    for command, action in hero.COMMANDS.items():
+        print("Press {} to {}".format(command, action[0]))
+    while True:
+        command = input("~~~~~~~Press key to continue~~~~~~~")
+        if command not in hero.COMMANDS:
+            print("Not a valid command")
+            continue
+        print("You are fighting "  + mob.name)
+        print("")
         time.sleep(1)
-        if mob.hp > 0:
-            MonsterAttack()
+        break
+    while True:
+        if command:
+            hero.COMMANDS[command][1]()
+            PlayerAttack()
             time.sleep(1)
-            if hero.hp <= 0:
-                print("++++++You were killed++++++")
+            if mob.hp > 0:
+                MonsterAttack()
                 time.sleep(1)
+                if hero.hp <= 0:
+                    print("++++++You were killed++++++")
+                    break
+                    time.sleep(1)
+                else:
+                    continue
+            elif mob.hp <= 0 and hero.hp > 0 :
+                print("++++++You killed " + mob.name+ "++++++")
+                print("")
+                print("Gained " + str(mob.exp) + " XP.")
+                print("")
+                hero.EXP += mob.exp
+                if hero.EXP >= hero.LVL_EXP:
+                    Level_Up()
+                    break
                 break
-            else:
-                continue
-        elif mob.hp <= 0 and hero.hp > 0 :
-            print("++++++You killed " + mob.name+ "++++++")
-            print("")
-            print("Gained " + str(mob.exp) + " XP.")
-            time.sleep(1)
-            break
-    Level_Up()
-
-# Encounter command
+                time.sleep(1)
