ef934089e2836747e82542d0ed095835ce113929
piotrkania <36471063+piotrkania@users.noreply.github.com>
2018-05-08 10:13:09 +0200

    added MANA attr

--- professions.py
+++ professions.py
@@ -2,47 +2,57 @@
 
 
 class Player:
-    def __init__(self,name,hp,ac,thaco):
-        self.name=name
-        self.hp=hp
-        self.ac=ac
-        self.thaco=thaco
+    def __init__(self, name, hp, ac, mana, thaco):
+        self.name = name
+        self.hp = hp
+        self.ac = ac
+        self.mana = mana
+        self.thaco = thaco
 
 class Fighter(Player):
     def __init__(self):
-        super().__init__(name=input("Whats Your name?: "),
-                         hp=12,ac=6,
-                         thaco=20)
+        super().__init__(name = input("Whats Your name?: "),
+                         hp =12, ac =6, mana = 0,
+                         thaco = 20)
     def fight():
         pass
 
-    PROF="FIGHTER"
-    MAX_HP=12
-    HD=8
-    LEVEL=1
-    EXP=1
-    LVL_EXP=4
-    POWER=[20]
+    PROF = "FIGHTER"
+    MAX_HP = 12
+    HD = 8
+    LEVEL = 1
+    EXP = 1
+    LVL_EXP = 4
+    MANA = 0
+    MAX_MANA = 0
+    POWER = [20]
     COMMANDS = {
         'f' : ('fight', fight)
     }
 
+
+
 class Rogue(Player):
     def __init__(self):
-        super().__init__(name=input("Whats Your name?: "),
-                         hp=9,ac=4,
-                         thaco=20)
+        super().__init__(name = input("Whats Your name?: "),
+                         hp = 9, ac = 4, mana = 0,
+                         thaco = 20)
 
     def fight():
         pass
+
     def backstab():
         pass
 
-    PROF="ROGUE"
-    MAX_HP=9
-    LEVEL=1
-    LEVEL_2=15
-    POWER=[1-7]
+    PROF = "ROGUE"
+    MAX_HP = 9
+    HD = 6
+    LEVEL = 1
+    EXP = 15
+    MANA = 0
+    MAX_MANA = 0
+    LVL_EXP = 10
+    POWER = [1, 2 , 3, 4, 5, 6, 7]
     COMMANDS = {
         'f' : ('fight', fight),
         'b' : ('backstab', backstab)
@@ -52,9 +62,9 @@ class Rogue(Player):
 
 class Mage(Player):
     def __init__(self):
-        super().__init__(name=input("Whats Your name?: "),
-                         hp=12,ac=7,
-                         thaco=20)
+        super().__init__(name = input("Whats Your name?: "),
+                         hp = 7,ac = 7, mana = 5,
+                         thaco = 20)
 
     def fight():
         pass
@@ -65,18 +75,21 @@ class Mage(Player):
     def cast_spell():
         pass
 
-    PROF="MAGE"
-    MAX_HP=5
-    LEVEL=1
-    LEVEL_2=10
-    MANA=1
-    MAX_MANA=1
-    POWER=[1,2,3,4]
+    PROF = "MAGE"
+    MAX_HP = 5
+    HD = 4
+    LEVEL = 1
+    EXP = 15
+    LVL_EXP = 10
+    MANA = 5
+    MAX_MANA = 5
+    POWER = [1, 2, 3, 4]
     COMMANDS = {
         'f' : ('fight', fight),
         's' : ('spell', cast_spell),
         'm' : ('mana', generate_mana)
     }
+
 
 def profession():
     while True:
