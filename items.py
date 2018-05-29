d7322148c4a416f5eb719cd94a20cd8b8a384cfd
piotrkania <36471063+piotrkania@users.noreply.github.com>
2018-05-29 13:50:30 +0200

    Update items.py
    PEP8 corrections

--- items.py
+++ items.py
@@ -1,31 +1,33 @@
-#!/usr/bin/python3
-
 class Item:
-   def __init__(self,name):
+    def __init__(self,name):
        self.name = name
 
-class HP_potion(Item):
+
+class HpPotion(Item):
     def __init__(self):
-        super().__init__(name = "HP Potion")
+        super().__init__(name="HP Potion")
 
     HP = 3
 
-class MP_potion(Item):
+
+class MpPotion(Item):
     def __init__(self):
-        super().__init__(name = "MP Potion")
+        super().__init__(name="MP Potion")
 
     MP = 3
 
+
 class Sword(Item):
     def __init__(self):
-        super().__init__(name = "Sword")
+        super().__init__(name="Sword")
 
     ATK = 1
 
+
 def equipment():
-    equipment = ["Sword", "MP Potion", "HP Potion"]
+    items = ["Sword", "MP Potion", "HP Potion"]
     while True:
-        for item in equipment:
+        for item in items:
             return item
 
 
